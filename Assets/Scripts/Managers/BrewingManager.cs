a9daf78820e31ccdbeebc46f90e50230bd8d9925
Dennis Abad <dennis.abad6@gmail.com>
2026-03-19 18:46:25 +1100

    Added bare buttons for prototyping Brewing A Coffee

---
+++ Assets/Scripts/Managers/BrewingManager.cs
@@ -0,0 +1,112 @@
+using System;
+using NSBLib.EventChannelSystem;
+using NSBLib.Helpers;
+using UnityEngine;
+
+public class BrewingManager : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField] private float beanAmount;
+    [SerializeField] private float beanMax = 10f;
+    [SerializeField] private float grindAmount;
+    [SerializeField] private float grindMax = 100f;
+    [SerializeField] private float heatAmount;
+    [SerializeField] private float heatMax = 100f;
+    [SerializeField] private float pourAmount;
+    [SerializeField] private float pourMax = 12f;
+    
+    [Header("Rate")]
+    [SerializeField] private float beanAmountPerSecond;
+    [SerializeField] private float grindAmountPerSecond;
+    [SerializeField] private float heatAmountPerSecond;
+    [SerializeField] private float coolingAmountPerSecond;
+    [SerializeField] private float pourAmountPerSecond;
+
+    [Header("Active")]
+    [SerializeField] private bool isGettingBeans;
+    [SerializeField] private bool isGrindingBeans;
+    [SerializeField] private bool isHeatingWater;
+    [SerializeField] private bool isPouringWater;
+    
+    [Header("Events")]
+    [SerializeField] private EventChannel<float> updateBeanValueText;
+    [SerializeField] private EventChannel<float> updateGrindValueText;
+    [SerializeField] private EventChannel<float> updateHeatValueText;
+    [SerializeField] private EventChannel<float> updatePourValueText;
+
+    private void Start()
+    {
+        beanAmount = 0;
+        grindAmount = 0;
+        heatAmount = 0;
+        pourAmount = 0;
+    }
+
+    public void IsBeansBtnPressed(bool isPressed)
+    {
+        isGettingBeans = isPressed;
+    }
+
+    public void IsGrindBtnPressed(bool isPressed)
+    {
+        isGrindingBeans = isPressed;
+    }
+
+    public void IsHeadBtnPressed(bool isPressed)
+    {
+        isHeatingWater = isPressed;
+    }
+
+    public void IsPourBtnPressed(bool isPressed)
+    {
+        isPouringWater = isPressed;
+    }
+
+    private void FixedUpdate()
+    {
+        AddBeans();
+        GrindBeans();
+        HeatWater();
+        PourWater();
+    }
+
+    private void PourWater()
+    {
+        if (isPouringWater && pourAmount < pourMax)
+        {
+            pourAmount += pourAmountPerSecond * Time.deltaTime;
+        }
+        updatePourValueText?.Invoke(pourAmount);
+    }
+
+    private void HeatWater()
+    {
+        if (isHeatingWater && heatAmount < heatMax)
+        {
+            heatAmount += heatAmountPerSecond * Time.deltaTime;
+        }
+        else if (heatAmount > 37f)
+        {
+            heatAmount -= coolingAmountPerSecond * Time.deltaTime;
+        }
+        updateHeatValueText?.Invoke(heatAmount);
+    }
+
+    private void AddBeans()
+    {
+        if (isGettingBeans && beanAmount < beanMax)
+        {
+            beanAmount += beanAmountPerSecond * Time.deltaTime;
+        }
+        updateBeanValueText?.Invoke(beanAmount);
+    }
+
+    private void GrindBeans()
+    {
+        if (isGrindingBeans && grindAmount < grindMax)
+        {
+            grindAmount += grindAmountPerSecond * Time.deltaTime;
+        }
+        updateGrindValueText?.Invoke(grindAmount);
+    }
+}
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,12 +1,38 @@
+using NSBLib.Helpers;
 using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text cashText;
+    [SerializeField] private TMP_Text beanValueText;
+    [SerializeField] private TMP_Text grindValueText;
+    [SerializeField] private TMP_Text heatValueText;
+    [SerializeField] private TMP_Text pourValueText;
 
     public void UpdateCashText(int cash)
     {
         cashText.text = $"${cash}";
+    }
+
+    public void UpdateBeanValueText(float value)
+    {
+        beanValueText.text = $"{value:F1} g";
+    }
+
+    public void UpdateGrindValueText(float value)
+    {
+        grindValueText.text = $"{value:F0}";
+    }
+
+    public void UpdateHeatValueText(float value)
+    {
+        heatValueText.text = $"{value:F0} c";
+    }
+
+    public void UpdatePourValueText(float value)
+    {
+        pourValueText.text = $"{value:F1} oz";
+        
     }
 }
